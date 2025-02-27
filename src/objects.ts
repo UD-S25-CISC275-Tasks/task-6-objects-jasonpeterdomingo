1d953d30456109b0e7fc0e6085e8a57f288ac36c
Jason Domingo <jayprdom@udel.edu>
2025-02-27 18:18:03 -0500

    completed duplicateQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -123,7 +123,13 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const duplicated: Question = {
+        ...oldQuestion,
+        id: id,
+        name: `Copy of ${oldQuestion.name}`,
+        published: false,
+    };
+    return duplicated;
 }
 
 /**
